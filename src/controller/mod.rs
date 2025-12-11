ea1607e0fa30a1c73597f80b2dcc6bcd0e64a0fe
burningtnt <pangyl08@163.com>
2025-12-11 20:22:50 +0800

    Drop support for legacy, pcl2ce typed rooms.

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -4,11 +4,11 @@ mod rooms;
 
 pub use rooms::*;
 
-use crate::scaffolding;
 pub use states::ExceptionType;
 pub use api::*;
+use crate::controller::rooms::scaffolding::protocols::HANDLERS;
 
 lazy_static::lazy_static! {
-    pub static ref SCAFFOLDING_PORT: u16 = scaffolding::server::start(experimental::HANDLERS, 13448)
-        .unwrap_or_else(|_| scaffolding::server::start(experimental::HANDLERS, 0).unwrap());
+    pub static ref SCAFFOLDING_PORT: u16 = crate::scaffolding::server::start(HANDLERS, 13448)
+        .unwrap_or_else(|_| crate::scaffolding::server::start(HANDLERS, 0).unwrap());
 }
